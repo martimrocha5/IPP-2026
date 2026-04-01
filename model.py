a0e448ccfa19ac0ec6df77fc2a8a485f8954752a
martimrocha5 <martimrocha5@gmail.com>
2026-04-01 21:28:10 +0100

    .

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import heapq
+
 class MotorAnalise:
     def __init__(self):
         self._regras_perfil = {
@@ -37,3 +39,44 @@ class MotorAnalise:
 
         peso_final = peso_base + penalizacao
         return max(1, peso_final)
+
+
+class MotorRecomendacao:
+    def __init__(self, motor_analise):
+        self._motor = motor_analise
+
+    def encontrar_melhor_caminho(self, rede, origem, destino, utilizador, modo="padrao"):
+        distancias = {origem: 0}
+        caminhos = {origem: []}
+        fila_prioridade = [(0, origem)]
+        visitados = set()
+
+        while fila_prioridade:
+            custo_atual, nodo_atual = heapq.heappop(fila_prioridade)
+
+            if nodo_atual in visitados:
+                continue
+            visitados.add(nodo_atual)
+
+            if nodo_atual == destino:
+                return caminhos[destino], custo_atual
+
+            for segmento in rede.obter_conexoes(nodo_atual):
+                vizinho = segmento.get_destino()
+                if vizinho in visitados:
+                    continue
+
+                peso_segmento = self._motor.calcular_peso_segmento(segmento, utilizador, modo)
+                novo_custo = custo_atual + peso_segmento
+
+                if vizinho not in distancias or novo_custo < distancias[vizinho]:
+                    distancias[vizinho] = novo_custo
+                    
+                    novo_caminho = list(caminhos[nodo_atual])
+                    novo_caminho.append(segmento)
+                    caminhos[vizinho] = novo_caminho
+                    
+                    heapq.heappush(fila_prioridade, (novo_custo, vizinho))
+
+        return None, float('inf')
+
