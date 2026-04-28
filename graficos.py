8bd30f5b9aa71e0fea2fe4283e2919b5b14353c5
martimrocha5 <martimrocha5@gmail.com>
2026-04-28 09:59:35 +0100

    .

--- graficos.py
+++ graficos.py
@@ -29,23 +29,19 @@ class VisualizacaoDados:
         largura = 0.35  
 
         fig, ax = plt.subplots(figsize=(12, 6))
-        
-        # Barras vermelhas para o perigo (poluição) e verdes para saúde (natureza)
+
         barras1 = ax.bar(x - largura/2, poluicao, largura, label='Poluição', color='#e74c3c')
         barras2 = ax.bar(x + largura/2, verdes, largura, label='Zonas Verdes', color='#2ecc71')
 
-        # 3. Formatação Profissional
         ax.set_ylabel('Índice de Qualidade (0-100)', fontweight='bold')
         ax.set_title('Raio-X Ambiental: Rede Braga Saudável', fontsize=16, fontweight='bold')
         ax.set_xticks(x)
         ax.set_xticklabels(nomes_ruas, rotation=0, fontsize=9)
         ax.legend()
 
-        # Adicionar os valores numéricos por cima de cada barra
         ax.bar_label(barras1, padding=3)
         ax.bar_label(barras2, padding=3)
 
-        # Ajustar as margens para nada ficar cortado
         fig.tight_layout()
         
         print("\n A abrir o painel de controlo visual... (Fecha a janela do gráfico para continuares a usar a consola)")
